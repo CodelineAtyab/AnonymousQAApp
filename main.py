038f3ea73052d86dbe0249b53e0efc658d67b6b6
CodelineAtyab <syed.atyab.hussain.om@gmail.com>
2024-07-21 11:38:27 +0500

    Updated the API to support MySQL for data sotrage

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
 import logging
+import traceback
 
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[
-                        logging.FileHandler("./logs/app.log"),
-                        logging.StreamHandler()
-                    ])
+from src.logging_service import logging
+from src.question_service import get_db_connection_instance
 
+
+# Instantiate a logger instance to write info and error level logs
 logger = logging.getLogger(os.path.basename(__file__))
 
 app = FastAPI()
@@ -33,17 +32,43 @@ dict_of_questions = {
 
 @app.get(path="/api/v1/questions")
 def get_all_questions():
-    logger.info("Returning all of the questions")
-    return dict_of_questions
+    return_value = {"data": None, "errMsg": None}
+    db_connection = get_db_connection_instance()
+    try:
+        logger.info("Returning all of the questions")
+        return_value["data"] = {key: value for key, value in db_connection.fetch_questions()}
+    except Exception:
+        return_value["errMsg"] = "Unable to retrieve questions"
+        logger.error(traceback.format_exc())
+    finally:
+        db_connection.disconnect()
+
+    return return_value
 
 
 @app.post(path="/api/v1/questions")
 async def add_question(request: Request):
+    return_value = {"data": None, "errMsg": None}
     recv_json_data = await request.json()
-
     logger.info(f"Creating a question based on {recv_json_data}")
-    dict_of_questions.update(recv_json_data)
-    return dict_of_questions
+
+    db_connection = get_db_connection_instance()
+    try:
+
+        for key, value in recv_json_data.items():
+            db_connection.insert_question(question_id=str(key), question_text=str(value))
+        return_value["data"] = recv_json_data
+    except Exception:
+        return_value["errMsg"] = "Unable to post the question"
+        logger.error(traceback.format_exc())
+        try:
+            db_connection.rollback()
+        except Exception:
+            logger.error(traceback.format_exc())
+    finally:
+        db_connection.disconnect()
+
+    return return_value
 
 
 if __name__ == "__main__":
